11a61484571f6e485ae71c27320b06adcd1d8eb3
sabinabialic <sabina.bialic@gmail.com>
2021-02-28 14:49:21 -0500

    Reformatting

--- assignment2.py
+++ assignment2.py
@@ -45,32 +45,28 @@ class Weight:
     medium = [5, 15, 20, 40]
     heavy = [20, 40, 100, 100]
 
-# Returns the Naive Bayes probability for a specified dog breed
-# Work in progress - something is off with the numbers
-def pNaiveBayes(breed, chars):
-    return ((pCharacteristic(chars[0], breed.girth[0], breed.girth[1]))
-         * (pCharacteristic(chars[1], breed.height[0], breed.height[1]))
-         * (pCharacteristic(chars[2], breed.weight[0], breed.weight[1]))
-         * breed.probability)
-# Testing
-#print (pNaiveBayes(Beagle, [59, 32, 17]))
-
-# Returns the probability of a characteristic for a specified dog breed
-# Work in progress - not sure what x is supposed to be
-def pCharacteristic(x, mean, sd):
-    variance = float(sd)**2
-    denom = (2*math.pi*variance)**0.5
-    num = math.exp(-(float(x)-float(mean))**2/(2*variance))
-    return num/denom
-# Testing
-#print(pCharacteristic(Beagle.probability, Beagle.girth[0], Beagle.girth[1]))
-
-# Fuzzy membership function
-def f(x):
-    return 'unimplemented'
-
 # Input is a three element list with [girth, height, weight]
 def naive_bayes_classifier(input):
+    # Returns the Naive Bayes probability for a specified dog breed
+    # Work in progress - something is off with the numbers
+    def pNaiveBayes(breed, chars):
+        return ((pCharacteristic(chars[0], breed.girth[0], breed.girth[1]))
+             * (pCharacteristic(chars[1], breed.height[0], breed.height[1]))
+             * (pCharacteristic(chars[2], breed.weight[0], breed.weight[1]))
+             * breed.probability)
+    # Testing
+    #print (pNaiveBayes(Beagle, [59, 32, 17]))
+
+    # Returns the probability of a characteristic for a specified dog breed
+    # Work in progress - not sure what x is supposed to be
+    def pCharacteristic(x, mean, sd):
+        variance = float(sd)**2
+        denom = (2*math.pi*variance)**0.5
+        num = math.exp(-(float(x)-float(mean))**2/(2*variance))
+        return num/denom
+    # Testing
+    #print(pCharacteristic(Beagle.probability, Beagle.girth[0], Beagle.girth[1]))
+
     # Returns the probability of each class in the order [beagle probability, corgi probability, husky probability, poodle probability]
     def class_probabilities():
       pBeagle = 0       # P(Beagle | input)
@@ -87,6 +83,10 @@ def naive_bayes_classifier(input):
 
 # Input is a three element list with [girth, height, weight]
 def fuzzy_classifier(input):
+    # Fuzzy membership function
+    def f(x):
+        return 'unimplemented'
+
     # Returns the highest membership class, either "beagle", "corgi", "husky", or "poodle"
     def highest_membership_class():
         return 'unimplemented'
